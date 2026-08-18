10556626ec93a0086154215f2d34e3acb21518e2
user <user@WIN-NDAPNIHP71N>
2026-08-18 13:14:59 +0900

    fix:深井 Task.25 javadocコメントの修正

--- trial_source/src/main/java/jp/co/sss/lms/service/StudentAttendanceService.java
+++ trial_source/src/main/java/jp/co/sss/lms/service/StudentAttendanceService.java
@@ -27,7 +27,7 @@ import jp.co.sss.lms.util.TrainingTime;
 /**
  * 勤怠情報（受講生入力）サービス
  * 
- * @author 東京ITスクール
+ * @author 東京ITスクール - Task.25
  */
 @Service
 public class StudentAttendanceService {
@@ -338,8 +338,9 @@ public class StudentAttendanceService {
 	
 	/**
 	 * 過去日の未入力チェック
-	 * @param date
-	 * @param 
+	 * @param lmsUserId
+	 * @return  未入力数
+	 * @throws ParseException
 	 */
 	public boolean notEnterCheck(Integer lmsUserId) throws ParseException{
 		//削除フラグを0にする
